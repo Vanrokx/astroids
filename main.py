c74c55e3e957e22a84b5ae55db633e0f79c9df42
Vance R <vrodgers@achieve.com>
2024-12-24 08:08:17 -0700

    Added player and sprite class

--- main.py
+++ main.py
@@ -3,11 +3,21 @@
 # throughout this fiele
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     screen_background = pygame.Color(0,0,0)
+
+    #Clock
+    clock = pygame.time.Clock()
+    dt = 0
+
+    #player init
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    #Game Loop
     while True:
         pygame.Surface.fill(screen, screen_background)
         #quit the game if the user clicks the X
@@ -15,11 +25,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
+        player.draw(screen)
         
         
         
-        
-        
+        time_passed = clock.tick(60)
+        dt = time_passed / 1000
         pygame.display.flip()
     # print("Starting asteroids!")
     # print("Screen width:",SCREEN_WIDTH)
